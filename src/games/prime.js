dceef28e8441e231fb2b5788fa5f2140d9caf9db
Bogdan <homoretsky@gmail.com>
2022-06-03 16:09:32 +0500

    Add isPrimeNumber function to prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,11 +3,16 @@ import startGame from '../index.js';
 
 const explanation = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+const isPrimeNumber = (number) => {
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) return false;
+  }
+  return true;
+};
 
 const generateRound = () => {
-  const randomNumber = generateRandomNumber(29);
-  const correctAnswer = primeNumbers.includes(randomNumber) ? 'yes' : 'no';
+  const randomNumber = generateRandomNumber(41, 2);
+  const correctAnswer = isPrimeNumber(randomNumber) ? 'yes' : 'no';
   const question = `Question: ${randomNumber}`;
 
   return [question, correctAnswer];
