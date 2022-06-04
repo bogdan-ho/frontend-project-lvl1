0f9ea976b8cee67b102e4333daf72e722e7dad83
Bogdan <homoretsky@gmail.com>
2022-06-04 12:51:39 +0500

    Update readability

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -6,15 +6,15 @@ const explanation = 'Find the greatest common divisor of given numbers.';
 const generateRound = () => {
   const randomNumber1 = generateRandomNumber(100);
   const randomNumber2 = generateRandomNumber(100);
-  const stack = [];
+  const commonDivisorsStack = [];
 
   for (let i = 1; i <= randomNumber1 && i <= randomNumber2; i += 1) {
     if (randomNumber1 % i === 0 && randomNumber2 % i === 0) {
-      stack.push(i);
+      commonDivisorsStack.push(i);
     }
   }
 
-  const correctAnswer = String(stack.pop());
+  const correctAnswer = String(commonDivisorsStack.pop());
   const question = `Question: ${randomNumber1} ${randomNumber2}`;
 
   return [question, correctAnswer];
